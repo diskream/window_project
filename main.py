7de2c07c9d874c076c12db7b6e1893771b0c2dcd
Маруся <71191784+dobrosha@users.noreply.github.com>
2022-06-14 17:55:32 +0300

    clustering

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class App(tk.Tk):
         self.HEIGHT = self.h // 2
         self.WIDTH = (7 * self.HEIGHT) // 9
         # self.geometry(f'{self.WIDTH}x{self.HEIGHT}')
-        self.title('Дипломный проект v0.068a')
+        self.title('Дипломный проект')
 
         self.table_list_frame = TopFrame(self)
         self.table_list_frame.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
